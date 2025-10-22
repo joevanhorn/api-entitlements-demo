0075780c2f4bbb1d23136b5820dd82c17019e355
joevanhorn <105666106+joevanhorn@users.noreply.github.com>
2025-10-22 18:43:29 -0500

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,59 +1,57 @@
 variable "aws_region" {
-  description = "AWS region to deploy resources"
+  description = "AWS region for deployment"
   type        = string
   default     = "us-east-1"
 }
 
-variable "environment" {
-  description = "Environment name"
-  type        = string
-  default     = "demo"
-}
-
 variable "domain_name" {
-  description = "Domain name for SCIM server (e.g., demo.yourdomain.com)"
+  description = "Domain name for the SCIM server"
   type        = string
 }
 
 variable "route53_zone_id" {
-  description = "Route53 hosted zone ID for your domain"
+  description = "Route53 hosted zone ID for the domain"
   type        = string
 }
 
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t2.micro"
-}
-
-variable "scim_auth_token" {
-  description = "SCIM authentication bearer token"
-  type        = string
-  sensitive   = true
-  default     = "demo-token-12345"
+  default     = "t3.micro"
 }
 
 variable "ssh_key_name" {
-  description = "Optional SSH key pair name for EC2 access"
+  description = "SSH key pair name for EC2 access"
   type        = string
   default     = ""
 }
 
 variable "allowed_ssh_cidr" {
-  description = "CIDR blocks allowed to SSH (0.0.0.0/0 = anywhere)"
+  description = "CIDR blocks allowed to SSH to the instance"
   type        = list(string)
   default     = ["0.0.0.0/0"]
 }
 
-variable "scim_basic_user" {
-  description = "Username for Basic Auth (SCIM)"
+variable "scim_auth_token" {
+  description = "Bearer token for SCIM authentication"
   type        = string
-  default     = "admin"
+  sensitive   = true
+}
+
+variable "scim_basic_user" {
+  description = "Basic auth username for SCIM"
+  type        = string
+  sensitive   = true
 }
 
 variable "scim_basic_pass" {
-  description = "Password for Basic Auth (SCIM)"
+  description = "Basic auth password for SCIM"
   type        = string
   sensitive   = true
-  default     = "password" # if empty, Basic won't be enabled unless you set it
+}
+
+variable "app_version" {
+  description = "Application version hash - used to force instance replacement when code changes"
+  type        = string
+  default     = "initial"
 }
